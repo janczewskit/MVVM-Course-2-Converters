8f67d8fc2696888f2a8e1140cb19f0fe81af88c0
janczewskit <janczewskit@op.pl>
2015-12-06 03:29:00 +0100

    Init

---
+++ Converters/MainWindowViewModelToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using MVVM_2_Converters.ViewModels;
+
+namespace MVVM_2_Converters.Converters
+{
+    public class MainWindowViewModelToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is MainWindowViewModel)
+            {
+                var mainWindowViewModel = value as MainWindowViewModel;
+                bool result = !string.IsNullOrEmpty(mainWindowViewModel.Login) 
+                    && !string.IsNullOrEmpty(mainWindowViewModel.Password);
+                return result ? Visibility.Visible : Visibility.Hidden;
+            }
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Input;
+using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
+
+namespace MVVM_2_Converters.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _login;
+
+        public string Login
+        {
+            get { return _login; }
+            set
+            {
+                _login = value;
+                IsVerifyTrue = this;
+                OnPropertyChanged(() => Login);
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                IsVerifyTrue = this;
+                OnPropertyChanged(() => Password);
+            }
+        }
+
+        private MainWindowViewModel _isVerifyTrue;
+
+        public MainWindowViewModel IsVerifyTrue
+        {
+            get { return this; }
+            set
+            {
+                _isVerifyTrue = value;
+                OnPropertyChanged(() => IsVerifyTrue);
+            }
+        }
+
+    }
+}
